6478d8961e04ce4964fb280b6c779ee6bcdb0e33
Mahmyda78 <mahmudaakter70000@gmail.com>
2025-08-25 00:21:34 +0600

    addGroups post

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.klnjmif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const groupsCollection = client.db('groupDB').collection('groups');
+   
+
+    // get
+    app.get('/groups', async (req, res) => {
+      const result = await groupsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Add a new group
+    app.post('/groups', async (req, res) => {
+    const newGroup = req.body;
+   console.log("Received group:", newGroup);
+   const result = await groupsCollection.insertOne(newGroup);
+   res.send(result);
+});
+
+
+    // test MongoDB connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hobby hub server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
